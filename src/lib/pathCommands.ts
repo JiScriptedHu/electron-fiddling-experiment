6b7feb4d4431b339fb9105a96c46c5bc8075a7f1
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2025-04-05 21:25:32 +0530

    Add command to print current directory

---
+++ src/lib/pathCommands.ts
@@ -0,0 +1,15 @@
+import * as vscode from 'vscode';
+
+export function registerPathCommands(context: vscode.ExtensionContext) {
+    const currPathCommand = vscode.commands.registerCommand('fiddle.getCurrentDirectory', () => {
+        const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+        
+        if (workspaceFolder) {
+            vscode.window.showInformationMessage(`Current workspace: ${workspaceFolder}`);
+        } else {
+            vscode.window.showWarningMessage('No workspace folder is currently open.');
+        }
+    });
+    
+    context.subscriptions.push(currPathCommand);
+}
